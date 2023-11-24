9f6eb5e4e1576d4ebaff11212f078d6aa5f1b353
ljs <qazplm807733@gmail.com>
2023-11-24 16:59:04 +0900

    mssql대응한 DB연결 완료. 사용자 로그인 일차구현

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::net::SocketAddr;
-
 use anyhow::Result;
+use std::net::SocketAddr;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-    sync::broadcast,
+    net::{
+        tcp::{ReadHalf, WriteHalf},
+        TcpListener,
+    },
+    sync::broadcast::{self},
 };
+use tokio_chat_server::db_tiberius;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    dotenvy::dotenv()?;
-
     // IP:PORT 주소 묶는다.
     let listener = TcpListener::bind("127.0.0.1:8090").await?;
 
@@ -22,8 +23,6 @@ async fn main() -> Result<(), anyhow::Error> {
     loop {
         // 클라이언트 접속을 허가한다 -> SOCKET 받아온다.
         let (mut socket, addr) = listener.accept().await?;
-
-        tx.send((format!("{} IP is comming!\n", addr).to_string(), addr))?;
 
         // tx를 하나 클론 떠온다.
         let tx = tx.clone();
@@ -38,8 +37,15 @@ async fn main() -> Result<(), anyhow::Error> {
 
             // BUFREADER를 통해 READHALF에 있는 버퍼를 읽어 들인다고 선언한다.
             let mut reader = BufReader::new(reader);
+
             // 버퍼 메시지를 저정할 공간
             let mut line = String::new();
+
+            check_db_logic(&mut writer, &mut reader, &mut line, &tx, &mut rx, &addr)
+                .await
+                .unwrap();
+
+            line.clear();
 
             // 버퍼 핸들링 루프 시작
             loop {
@@ -50,7 +56,7 @@ async fn main() -> Result<(), anyhow::Error> {
                     // 리더로 엔터 전까지 한줄을 읽는다. 그걸 메시지 저장공간에 저장한다.
                     res = reader.read_line(&mut line) => {
                         if res.unwrap() == 0 {
-                            break;
+                            return Ok::<(), anyhow::Error>;
                         }
                         // 메시지 버퍼와 IP 주소를 보낸다.
                         tx.send((line.clone(), addr)).unwrap();
@@ -73,3 +79,82 @@ async fn main() -> Result<(), anyhow::Error> {
         });
     }
 }
+/// ## Title : 
+/// 
+///     DB 체크해서 개인한테 보내주고 전체에게 얘 로그인 했다고 알린다.
+///
+/// ## Parameters : 
+/// 
+///     writer: &mut WriteHalf<'a>
+///     쓰는 소켓(reader와 같은 수명)
+/// 
+///     reader: &mut BufReader<ReadHalf<'a>>
+///     읽는 소켓(writer와 같은 수명) 
+/// 
+///     text: &mut String
+///     텍스트 저장할 버퍼
+/// 
+///     tx: &broadcast::Sender<(String, SocketAddr)>
+///     broadcast channel Sender 전체 문자 보낼 sender
+/// 
+///     rx: &mut broadcast::Receiver<(String, SocketAddr)>
+///     broadcast channel Receiver 전체 문자 받아서 뿌려줄 receiver
+/// 
+///     addr: &SocketAddr 
+///     유저 IP:Port 정보
+///
+/// ## Return : 
+/// 
+///     로그인 성공인지 실패인지에 대한 db 처리 결과를 그대로 보낸다.
+///     Aysnc Result<bool, anyhow::Error>
+/// 
+/// ## 수정 내역 :
+/// 
+///     23.11.24
+///     아이디 비빌번호 가져와서 체크하고 전체문자 보내는 것 까지 init
+async fn check_db_logic<'a>(
+    writer: &mut WriteHalf<'a>,
+    reader: &mut BufReader<ReadHalf<'a>>,
+    text: &mut String,
+    tx: &broadcast::Sender<(String, SocketAddr)>,
+    rx: &mut broadcast::Receiver<(String, SocketAddr)>,
+    addr: &SocketAddr,
+) -> Result<bool, anyhow::Error> {
+    // 아이디를 입력해 주세요 -> todo!(space 비밀번호 식으로 해서 같이 받자.) 
+    writer
+        .write_all("아이디를 입력하여 주세요\n".as_bytes())
+        .await?;
+
+    // bufreader 입히고
+    let mut reader = BufReader::new(reader);
+
+    // 로그인정보 입력받는다.
+    reader.read_line(text).await?;
+
+    // 로그인 체크 함수 call 해주고
+    let login_bool = db_tiberius(&text.trim().to_string(), &"1234".to_string()).await?;
+
+    // true면
+    if login_bool  {
+        // 처음 입장 할 때 전체에게 인사 ?
+        tx.send((
+            format!("{} 님이 입장 하였습니다.\n", addr).to_string(),
+            *addr,
+        ))
+        .unwrap();
+
+        // 전체한테 전달하고
+        let (msg, other_addr) = rx.recv().await.unwrap();
+
+        // 내용 뿌린다.
+        writer
+            .write_all(format!("{} : {}", other_addr, msg).as_bytes())
+            .await
+            .unwrap();
+    }
+    
+    // 최종 로그인 정보 리턴해준다 -> todo!(나중에는 아이디 정보 넘겨주는 걸로 해서 표시나 로그아웃 가능하게)
+    Ok(login_bool)
+}
+
+// async fn broadcast_all_of_them() {}
---
+++ src/tibe/lib.rs
@@ -0,0 +1,89 @@
+use anyhow::Result;
+use tiberius::{AuthMethod, Client, Config, Query};
+use tokio::net::TcpStream;
+use tokio_util::compat::TokioAsyncWriteCompatExt;
+
+/// ## Title: 
+/// 
+///     tiberius 를 통한 로그인 체크
+///
+/// ## Parameters : 
+/// 
+///     user_id: &string
+///     유저 아이디
+/// 
+///     user_pw: &string 
+///     유저 비밀번호 
+///
+/// ## Return : 
+/// 
+///     로그인 성공인지 실패인지에 대한 결과
+///     Aysnc Result<bool, anyhow::Error>
+/// 
+/// ## 수정 내역 :
+/// 
+///     23.11.24
+///     아이디 비빌번호 가져오는 기본적인 함수 init
+pub async fn db_tiberius(user_id: &String, user_pw: &String) -> Result<bool, anyhow::Error> {
+    // ENV에 DB 정보 가져온다.
+    dotenvy::dotenv().unwrap();
+
+    // DB 정보란 init
+    let mut config = Config::new();
+
+    // IP:port 정보 입력해준다.
+    config.host("127.0.0.1");
+    config.port(1433);
+
+    // 보안정보(아이디 비밀번호) 입력해준다.
+    config.authentication(AuthMethod::sql_server(
+        dotenvy::var("USER_ID").unwrap(),
+        dotenvy::var("USER_PW").unwrap(),
+    ));
+
+    // 인증서 무시한다는 뜻
+    config.trust_cert();
+
+    // DB Tcp 연결 위한 요청 해준다.
+    let tcp = TcpStream::connect(config.get_addr()).await?;
+
+    // Nagle Algo를 무시한다.(스트림을 있는 그래로 기다리지 않고 보낸다.)
+    // Nagle Algo는 https://devjh.tistory.com/106 참조 함.
+    tcp.set_nodelay(true)?;
+
+    // DB 주소로 TCP 연결 해준다.
+    let mut client = Client::connect(config, tcp.compat_write()).await?;
+
+    // 데이터 가져오기 위한 결과 파라미터 매칭 (숫자로 해도 상관은 없나보다. 근데 좀더 명확하게 표현하려고)
+    let params = vec![String::from("CHAT_USER"), String::from("CHAT_PW")];
+
+    // 적용할 sql문
+    let sql_query_string = format!(
+        "SELECT CHAT_USER, CHAT_PW \
+        FROM    CHAT_SERVER.dbo.TB_CHAT_USER \
+        WHERE   CHAT_USER = '{}' \
+        AND     CHAT_PW = '{}'",
+        user_id, user_pw
+    );
+
+    // 쿼리 만들어내고
+    let mut select_query = Query::new(sql_query_string);
+
+    // 파라미터 입혀서 데이터 가져올 수 있게 하고
+    for param in params {
+        select_query.bind(param);
+    }
+
+    // excute 한다.
+    let res = select_query.query(&mut client).await?;
+
+    // row 데이터 가져오면
+    let row = res.into_row().await?.unwrap();
+
+    // 데이터 있는지 체크하고 리턴
+    if row.len() > 0 {
+        return Ok(true);
+    }
+
+    Ok(false)
+}
